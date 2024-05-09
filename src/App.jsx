ec918bfb6679ef6590e5a71ff146b426a7f61f1c
Jefflobao <jefferson_lobao@hotmail.com>
2024-05-09 12:08:28 -0300

    inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+import { Button } from "primereact/button"
+
 const App = () => {
   return ( 
     <>
-    
+
+    <Button label="{teste}" />
+
     </>
    );
 }
